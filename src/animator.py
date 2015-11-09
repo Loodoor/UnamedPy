2f3fe060268ce063142eb1861013ac2dd183f854
Loodoor <folaefolc@outlook.fr>
2015-11-09 10:24:14 +0100

    Animation verticale ou horizontale

--- src/animator.py
+++ src/animator.py
@@ -4,12 +4,13 @@ import pygame
 
 
 class BaseAnimator:
-    def __init__(self, base_image: pygame.Surface, velocity: float):
+    def __init__(self, base_image: pygame.Surface, velocity: float, vertical):
         self.base_image = base_image
         self.velocity = velocity
         self.decalage = int(self.base_image.get_width() // self.velocity + 1)
         self.output = []
         self.time = 0
+        self.vertical = vertical
 
     def load(self):
         for i in range(self.decalage):
@@ -17,8 +18,12 @@ class BaseAnimator:
             self.surf.fill((76, 76, 76))
             self.surf.set_colorkey((76, 76, 76))
 
-            self.surf.blit(self.base_image, (self.time - self.base_image.get_width(), 0))
-            self.surf.blit(self.base_image, (self.time, 0))
+            if not self.vertical:
+                self.surf.blit(self.base_image, (self.time - self.base_image.get_width(), 0))
+                self.surf.blit(self.base_image, (self.time, 0))
+            else:
+                self.surf.blit(self.base_image, (0, self.time - self.base_image.get_height()))
+                self.surf.blit(self.base_image, (0, self.time))
 
             self.surf.convert_alpha()
 
