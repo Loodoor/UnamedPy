f4146870859bd85b1d4b5c97477370fcd5f9c9c8
loodoor@ubuntu <folaefolc@outlook.fr>
2016-05-01 11:37:17 +0200

    correctif launchers

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,6 @@
 # launcher du jeu
 cd src/
-python3 main.py "err_log.log" 2>&1
+python3 main.py >"err_log.log" 2>&1
 echo "Si le jeu se coupe brutalement et que vous pensez que cela est dû à un bug,"
 echo "je vous serais reconnaissant de m'envoyer le contenu du fichier qui va s'ouvrir"
 gedit "err_log.log"
