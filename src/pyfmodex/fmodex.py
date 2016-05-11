1f859b37476ba7a1bd025cfe2f4cd777e30ea0fa
Loodoor <folaefolc@outlook.fr>
2016-05-11 18:39:42 +0200

    fmod ex binding

--- src/pyfmodex/fmodex.py
+++ src/pyfmodex/fmodex.py
@@ -1,20 +1,26 @@
 from ctypes import *
-import os, platform
-arch = platform.architecture()[0]
-if os.name == 'nt':
-    if arch == "32bit":
-        _dll = windll.fmodex
-    else:
-        _dll = windll.fmodex64
-elif os.name == "posix":
-    if arch == "32bit":
-        _dll = CDLL('libfmodex.so')
-    else:
-        _dll = CDLL('libfmodex64.so')
+import os
+import platform
 from . import globalvars
-globalvars.dll = _dll
 from .utils import ckresult
 from .structobject import Structobject as so
+
+
+arch = platform.architecture()[0]
+
+if os.name == 'nt':
+    if arch == "32bit":
+        _dll = CDLL('../assets/lib/fmod.dll')
+    else:
+        _dll = CDLL('../assets/lib/fmod64.dll')
+elif os.name == "posix":
+    if arch == "32bit":
+        _dll = CDLL('../assets/lib/libfmodex.so')
+    else:
+        _dll = CDLL('../assets/lib/libfmodex64.so')
+
+globalvars.dll = _dll
+
 
 def get_debug_level():
     """Returns the current debug level.
@@ -24,11 +30,13 @@ def get_debug_level():
     ckresult(_dll.FMOD_Debug_GetLevel(byref(level)))
     return level.value
 
+
 def set_debug_level(level):
     """Sets the current debug level.
     :param level: The level to set.
     """
     ckresult(_dll.FMOD_Debug_SetLevel(level))
+
 
 def get_disk_busy():
     """Gets the busy status of the disk.
@@ -39,12 +47,14 @@ def get_disk_busy():
     ckresult(_dll.FMOD_File_GetDiskBusy(byref(busy)))
     return busy.value
 
+
 def set_disk_busy(busy):
     """Sets the busy status.
     :param busy: The busy status.
     :type busy: boolean
 """
     ckresult(_dll.FMOD_File_SetDiskBusy(busy))
+
 
 def get_memory_stats(blocking):
     """Returns the current memory stats.
